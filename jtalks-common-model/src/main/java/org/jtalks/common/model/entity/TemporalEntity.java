ef4efa8101facfb510da911122d38fb80dc531cb
diewindowsdie <diewindowsdie@gmail.com>
2011-09-09 13:42:17 +0400

    #JTALKSCOMMON-9 Added class TemporalEntity and unit tests for it.

---
+++ jtalks-common-model/src/main/java/org/jtalks/common/model/entity/TemporalEntity.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (C) 2011  jtalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Also add information on how to contact you by electronic and paper mail.
+ * Creation date: Apr 12, 2011 / 8:05:19 PM
+ * The jtalks.org Project
+ */
+package org.jtalks.common.model.entity;
+
+import org.joda.time.DateTime;
+
+/**
+ * This class represents an abstract entity that has creation date and modification date.
+ * Date: 09.09.2011<br />
+ * Time: 12:48
+ *
+ * @author Alexey Malev
+ */
+public abstract class TemporalEntity extends Entity {
+
+    private DateTime creationDate;
+    private DateTime modificationDate;
+
+    public TemporalEntity() {
+        DateTime entityCreationDate = new DateTime();
+
+        this.creationDate = entityCreationDate;
+        this.modificationDate = entityCreationDate;
+    }
+
+    /**
+     * This method updates entity modification date to current date.
+     */
+    public final void updateModificationDate() {
+        this.modificationDate = new DateTime();
+    }
+
+    /**
+     * This method returns the date when the entity was created.
+     *
+     * @return Date when entity was created.
+     */
+    public DateTime getCreationDate() {
+        return this.creationDate;
+    }
+
+    /**
+     * <p>This method sets entity creation date to specified value.</p>
+     * <p>Method is protected so only Hibernate has access to it.</p>
+     *
+     * @param creationDate New entity creation date
+     */
+    protected void setCreationDate(DateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    /**
+     * This method returns entity last modification date.
+     *
+     * @return Last modification date of the entity.
+     */
+    public DateTime getModificationDate() {
+        return this.modificationDate;
+    }
+
+    /**
+     * <p>This method sets entity modification date to specified value.</p>
+     *
+     * @param modificationDate New entity modification date.
+     */
+    protected void setModificationDate(DateTime modificationDate) {
+        this.modificationDate = modificationDate;
+    }
+}
---
+++ jtalks-common-model/src/test/java/org/jtalks/common/model/entity/TemporalEntityTest.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (C) 2011  jtalks.org Team
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Also add information on how to contact you by electronic and paper mail.
+ * Creation date: Apr 12, 2011 / 8:05:19 PM
+ * The jtalks.org Project
+ */
+package org.jtalks.common.model.entity;
+
+import org.joda.time.DateTime;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * This class contains unit tests for {@link TemporalEntity}
+ * Date: 09.09.2011<br />
+ * Time: 13:00
+ *
+ * @author Alexey Malev
+ */
+public class TemporalEntityTest {
+
+    private class SomeEntity extends TemporalEntity {
+
+    }
+
+    private SomeEntity sut;
+
+    @BeforeMethod
+    public void setUp() {
+        sut = new SomeEntity();
+    }
+
+    @Test
+    public void testConstructorSetsDates() {
+        assertNotNull(sut.getCreationDate());
+        assertNotNull(sut.getModificationDate());
+    }
+
+    @Test
+    public void testConstructorSetsEqualDates() {
+        assertEquals(sut.getCreationDate(), sut.getModificationDate());
+    }
+
+    @Test
+    public void testSetCreationDate() {
+        DateTime currentCreationDate = sut.getCreationDate();
+        sut.setCreationDate(new DateTime(System.currentTimeMillis() + 100000));
+
+        assertNotSame(sut.getCreationDate(), currentCreationDate);
+    }
+
+    @Test
+    public void testSetModificationDate() {
+        DateTime currentModificationDate = sut.getModificationDate();
+        sut.setModificationDate(new DateTime(System.currentTimeMillis() + 100000));
+
+        assertNotSame(sut.getModificationDate(), currentModificationDate);
+    }
+
+    @Test
+    public void testUpdateModificationDate() {
+        try {
+            DateTime currentModificationDate = sut.getModificationDate();
+            Thread.sleep(50);
+            sut.updateModificationDate();
+            assertNotSame(sut.getModificationDate(), currentModificationDate);
+        }
+        catch (InterruptedException e) {
+            throw new IllegalStateException("InterruptedException shouldn't be thrown here.", e);
+        }
+    }
+}
