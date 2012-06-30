2edc8677499f1a88e8d157ab0caba8b9779c1bd2
Ancient_Mariner <xander.lysak@gmail.com>
2012-06-30 13:38:17 +0300

    #JTALKSCOMMON-35 * Comments for ACL structure

--- jtalks-common-model/src/main/resources/org/jtalks/common/migrations/V3__ACL_tables.sql
+++ jtalks-common-model/src/main/resources/org/jtalks/common/migrations/V3__ACL_tables.sql
@@ -10,7 +10,7 @@ set foreign_key_checks=0;
 DROP TABLE IF EXISTS `acl_sid`;
 CREATE TABLE `acl_sid` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `principal` tinyint(1) NOT NULL,                         -- a label, that shows whether the object is principal(is there a possibility to login with this object or not, i.e. groups cannot be here)
+  `principal` tinyint(1) NOT NULL,                         -- a label, that shows whether the object is principal(is there a possibility to login with this object or not, i.e. groups cannot be here; true indicates that the sid is a user, false means that the sid is a granted authority)
   `sid` varchar(100) NOT NULL,                             -- an entity, which get the permissions()
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_acl_sid` (`sid`,`principal`)
@@ -38,6 +38,7 @@ CREATE TABLE `acl_class` (
 -- Table structure for table `acl_entry`
 --
 -- Contains the logs of security journal. Each log contains the permissions, that is allowed/not allowed for concrete object
+-- Helps to specify what actions can be performed on each of these objects by the desired users
 --
 DROP TABLE IF EXISTS `acl_entry`;
 CREATE TABLE `acl_entry` (
@@ -46,9 +47,9 @@ CREATE TABLE `acl_entry` (
   `ace_order` int(11) NOT NULL,                                   -- an order of permissions priority (AccessControlEntry (ACE) represents individual permission assignment)
   `sid` bigint(20) NOT NULL,                                      -- a link to a acl_sid table - who gets the permissions
   `mask` int(11) NOT NULL,                                        -- bit mask, which identifies the permissions (for example for read, write etc.)
-  `granting` tinyint(1) NOT NULL,                                 -- a value, that identifies whether the permission is allowed or not (for JCommune - 1 - allowed, 0 - not allowed)
-  `audit_success` tinyint(1) NOT NULL,
-  `audit_failure` tinyint(1) NOT NULL,
+  `granting` tinyint(1) NOT NULL,                                 -- a value, that identifies whether the permission is allowed or not (for JCommune - 1 - allowed, 0 - not allowed). The granting field, if set to true, indicates that the permissions indicated by mask are granted to the corresponding sid, otherwise they are revoked or blocked.
+  `audit_success` tinyint(1) NOT NULL,                            -- A flag to indicate whether to audit a successful permission
+  `audit_failure` tinyint(1) NOT NULL,                            -- A flag to indicate whether to audit a failed permission
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_acl_entry` (`acl_object_identity`,`ace_order`),
   KEY `fk_acl_entry_obj_id` (`acl_object_identity`),
@@ -60,7 +61,8 @@ CREATE TABLE `acl_entry` (
 --
 -- Table structure for table `acl_object_identity`
 --
--- Each table entry is the representation of the object. for which the permissions are given
+-- Each table entry is the representation of the object (in other words - acl_class is class and acl_object_identity is instance of acl_class).
+-- for which the permissions are given
 --
 DROP TABLE IF EXISTS `acl_object_identity`;
 CREATE TABLE `acl_object_identity` (
@@ -68,7 +70,7 @@ CREATE TABLE `acl_object_identity` (
   `object_id_class` bigint(20) NOT NULL,                              -- a link to acl_class table - the type of the object
   `object_id_identity` bigint(20) NOT NULL,                           -- an id of the object (for JCommune - the ID of the object)
   `parent_object` bigint(20) DEFAULT NULL,                            -- a link to a parent object
-  `owner_sid` bigint(20) DEFAULT NULL,                                -- a link to a acl_sid table - the owner of the object
+  `owner_sid` bigint(20) DEFAULT NULL,                                -- a link to a acl_sid table - the owner of the object(each object must have an owner). Pay attention if object was inherited - the fields parent_object and entries_inheriting are used to give the due details
   `entries_inheriting` tinyint(1) NOT NULL,                           -- gives the key whether the parent permissions should be extended to children
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_acl_objid` (`object_id_class`,`object_id_identity`),
